36fecc900f76766638ddbfe8bb252a7e26c4f9a6
Yashash Gaurav <yashash.gaurav@gmail.com>
2022-04-26 10:44:09 -0400

    cuz I am lazy 2

--- extras/create_folder_names.py
+++ extras/create_folder_names.py
@@ -1,8 +1,31 @@
-name = ''
-for s in "121: Best Time to Buy and Sell Stock":
-    if s.isalnum():
-        name += s
-    if s == ' ':
-        name += '_'
+from pathlib import Path
 
-print(name)
+
+def runner(problem_title: str):
+    '''
+    TODO: add ability to add links and comments to the
+    solution folder's sol file
+    '''
+    folder_name = ""
+
+    for s in "121: Best Time to Buy and Sell Stock":
+        if s.isalnum():
+            folder_name += s
+        if s == " ":
+            folder_name += "_"
+
+    p = Path(
+        str(Path(__file__).parent.parent) + "/problems" + "/" + folder_name
+    )
+    p.mkdir(parents=True, exist_ok=True)
+    
+    exp_fn = 'experiments.py'
+    exp_path = p / exp_fn
+    with exp_path.open("w", encoding ="utf-8") as f:
+        f.write(f"""'''
+    {problem_title}
+'''
+        """)
+
+
+runner("121. Best Time to Buy and Sell Stock")
