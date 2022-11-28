f63146cec0c28148007bfa081c9c1d606187b5d7
Yashash Gaurav <yashash.gaurav@gmail.com>
2022-11-28 02:16:15 -0500

    fixing formatting

--- problems/253_Meeting_Rooms_II/solution.py
+++ problems/253_Meeting_Rooms_II/solution.py
@@ -1,23 +1,19 @@
-'''
+"""
     253. Meeting Rooms II
-'''
+"""
 
 from typing import List
 
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        
-        max_meetings = 0
-        
+
         start = sorted([x[0] for x in intervals])
         end = sorted([x[1] for x in intervals])
-        
-        
-        max_meets, current_meets = 0, 0 
-        sp, ep = 0, 0    
-        
-        
+
+        max_meets, current_meets = 0, 0
+        sp, ep = 0, 0
+
         while sp < len(start):
             if start[sp] < end[ep]:
                 sp += 1
@@ -26,8 +22,8 @@ class Solution:
                 ep += 1
                 current_meets -= 1
             max_meets = max(max_meets, current_meets)
-        
-        return max_meets
-            
 
-print(Solution().minMeetingRooms([[0,30],[5,10],[15,20]])) # 2
+        return max_meets
+
+print(Solution().minMeetingRooms([[0, 30], [5, 10], [15, 20]]))  # 2
+
