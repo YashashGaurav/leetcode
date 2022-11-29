fc89a3a2f322b500a115ed8f54272a5b3447033e
Yashash Gaurav <yashash.gaurav@gmail.com>
2022-11-28 21:20:35 -0500

    neetcoding 198

--- problems/198_House_Robber/solution.py
+++ problems/198_House_Robber/solution.py
@@ -6,7 +6,9 @@ from typing import List
 
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    
+    # Accepted	69 ms	13.8 MB
+    def rob0(self, nums: List[int]) -> int:
 
         if len(nums) <= 2:
             return max(nums)
@@ -18,6 +20,19 @@ class Solution:
             max_rob = max(max_rob, nums[i])
 
         return max_rob
+
+    # Accepted	46 ms	13.9 MB	
+    # neetcode style - accumulation of results
+    def rob(self, nums: List[int]) -> int:
+        
+        rob_1, rob_2 = 0, 0
+        
+        for i in range(len(nums) - 1, -1, -1):
+            temp = max(nums[i] + rob_2, rob_1)
+            rob_2 = rob_1
+            rob_1 = temp
+        
+        return rob_1
 
 
 print(Solution().rob([2, 7, 9, 3, 1]))  # 12
