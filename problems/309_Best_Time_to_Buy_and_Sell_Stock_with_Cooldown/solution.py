92dc74982298a3852a8384375a98e71032c481d8
Yashash Gaurav <yashash.gaurav@gmail.com>
2022-12-13 13:27:30 -0500

    solved 309

--- problems/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution.py
+++ problems/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution.py
@@ -1,34 +1,35 @@
-'''
+"""
     309. Best Time to Buy and Sell Stock with Cooldown
-'''
+"""
+
 
 class Solution:
     # Accepted	103 ms	17.8 MB
     def maxProfit(self, prices: List[int]) -> int:
 
-        self.max_time = len(prices)        
+        self.max_time = len(prices)
         dp = {}
 
         def dfs(time_p, isBought):
-            
+
             if time_p >= self.max_time:
                 return 0
-            
+
             if (time_p, isBought) in dp:
                 return dp[(time_p, isBought)]
-            
-            if not isBought: # can buy
+
+            if not isBought:  # can buy
                 profit = max(
-                    dfs(time_p + 1, not isBought) - prices[time_p], # buying
-                    dfs(time_p + 1, isBought) # cooldown
+                    dfs(time_p + 1, not isBought) - prices[time_p],  # buying
+                    dfs(time_p + 1, isBought),  # cooldown
                 )
-            if isBought: # can sell
+            if isBought:  # can sell
                 profit = max(
-                    dfs(time_p + 2, not isBought) + prices[time_p], # selling
-                    dfs(time_p + 1, isBought), # cooldown
+                    dfs(time_p + 2, not isBought) + prices[time_p],  # selling
+                    dfs(time_p + 1, isBought),  # cooldown
                 )
-            
+
             dp[(time_p, isBought)] = profit
             return dp[(time_p, isBought)]
-        
+
         return dfs(0, False)
