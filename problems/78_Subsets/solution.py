548d1e8013a5e7caed7b37cc5b468cf9a8d61f2d
Yashash Gaurav <ygaurav@ripple.com>
2025-02-11 00:00:10 -0800

    solved 78

---
+++ problems/78_Subsets/solution.py
@@ -0,0 +1,52 @@
+'''
+    78. Subsets
+'''
+from typing import List
+
+class Solution:
+
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+
+    #     return_sets = [[]]
+
+    #     for index in range(len(nums)):
+            
+
+    #     return return_sets
+
+    # backtracking - Trees
+    def subsets_1(self, nums: List[int]) -> List[List[int]]:
+        
+        return_sets = []
+
+        def dfs(index, current_subset):
+
+            if index >= len(nums):
+                return_sets.append(current_subset.copy())
+                return
+            
+            # left case - adding to subset
+            current_subset.append(nums[index])
+            dfs(index+1, current_subset)
+
+            # right case - not adding the subset
+            current_subset.pop()
+            dfs(index+1, current_subset)
+        
+        dfs(0, [])
+        return return_sets
+        
+
+    
+    def subset_0(self, nums):
+        pass
+        
+        # for subset_len in range(1, len(nums)+1):
+        #     for i in range(0, len(nums) - subset_len + 1):
+        #         return_sets.append(nums[i:i+subset_len])
+        
+        # return return_sets
+
+
+print(Solution().subsets(nums = [1,2,3]))
+print(Solution().subsets(nums = [0]))
