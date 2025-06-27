598be1df5cd6cb9b1f0e9b6d88bdf8a2e93f6ddd
Yashash Gaurav <yashash.gaurav@gmail.com>
2025-06-26 21:01:13 -0700

    solved 45

---
+++ problems/45_Jump_Game_II/solution.py
@@ -0,0 +1,23 @@
+"""
+45. Jump Game II
+"""
+
+from typing import List
+
+
+class Solution:
+    # 79.42% | 56.56%
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            l = r + 1
+            r = farthest
+            jumps += 1
+
+        return jumps
